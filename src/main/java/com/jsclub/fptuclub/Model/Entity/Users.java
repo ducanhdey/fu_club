7c3d86baee33ad50d45cdf30422ffd23481db16b
unknown <thuylinh@gmail.com>
2024-03-12 16:22:26 +0700

    commit ngay 12/3

--- src/main/java/com/jsclub/fptuclub/Model/Entity/Users.java
+++ src/main/java/com/jsclub/fptuclub/Model/Entity/Users.java
@@ -1,7 +1,6 @@
 package com.jsclub.fptuclub.Model.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
